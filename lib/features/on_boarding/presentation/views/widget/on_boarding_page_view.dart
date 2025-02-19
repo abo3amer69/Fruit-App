daf2ab94d02bcabb876776c72efefbab5a97c4a5
Ahmed Amer <01099517574med@gmail.com>
2025-02-19 19:09:12 +0200

    page view item

--- lib/features/on_boarding/presentation/views/widget/on_boarding_page_view.dart
+++ lib/features/on_boarding/presentation/views/widget/on_boarding_page_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:fruit_app/core/utils/app_images.dart';
+import 'package:fruit_app/features/on_boarding/presentation/views/widget/page_view_item.dart';
 
 class OnBoardingPageView extends StatelessWidget {
   const OnBoardingPageView({super.key});
@@ -6,7 +8,35 @@ class OnBoardingPageView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return PageView(
-      
+      children: [
+        PageViewItem(
+          image: Assets.assetsImagesPageViewItem1Image,
+          backgroundImage: Assets.assetsImagesPageViewItem1BackgroundImage,
+          subTitle:
+              'اكتشف تجربة تسوق فريدة مع FruitHUB. استكشف مجموعتنا الواسعة من الفواكه الطازجة الممتازة واحصل على أفضل العروض والجودة العالية.',
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [Text('مرحبًا بك في'), Text('Fruit'), Text('HUB')],
+          ),
+        ),
+
+        PageViewItem(
+          image: Assets.assetsImagesPageViewItem2Image,
+          backgroundImage: Assets.assetsImagesPageViewItem2BackgroundImage,
+          subTitle:
+              'نقدم لك أفضل الفواكه المختارة بعناية. اطلع على التفاصيل والصور والتقييمات لتتأكد من اختيار الفاكهة المثالية',
+          title: Text(
+            'ابحث وتسوق',
+            style: TextStyle(
+              color: Color(0xff0C0D0D),
+              fontSize: 23,
+              fontFamily: 'Cairo',
+              fontWeight: FontWeight.w700,
+              height: 0,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:fruit_app/core/helper_function/on_generate_routes.dart';
 import 'package:fruit_app/features/splash/presentation/views/splash_view.dart';
+
+import 'generated/l10n.dart';
 
 void main() {
   runApp(const FruitHup());
 }
+
 class FruitHup extends StatelessWidget {
   const FruitHup({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
+      locale: Locale('ar'),
       debugShowCheckedModeBanner: false,
       onGenerateRoute: onGenerateRoute,
       initialRoute: SplashView.routeName,
