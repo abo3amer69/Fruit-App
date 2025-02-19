5bbf9ec60c32ae1c7b0cce13a4fad5ff1f2b1a03
Ahmed Amer <01099517574med@gmail.com>
2025-02-19 16:56:13 +0200

    create on generate route and create splsh view ui

---
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:fruit_app/features/splash/presentation/views/widgets/splash_view_body.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  static const routeName = 'splash';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SplashViewBody(),
+    );
+  }
+}
