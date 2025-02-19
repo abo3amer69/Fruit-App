9f4ffa4809a43cb01dbfa5c8c4991a747deb7c85
Ahmed Amer <01099517574med@gmail.com>
2025-02-19 21:07:52 +0200

    create dots indicator

--- lib/features/on_boarding/presentation/views/widget/on_boarding_view_body.dart
+++ lib/features/on_boarding/presentation/views/widget/on_boarding_view_body.dart
@@ -1,4 +1,6 @@
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:fruit_app/core/utils/app_colors.dart';
 import 'package:fruit_app/features/on_boarding/presentation/views/widget/on_boarding_page_view.dart';
 
 class OnBoardingViewBody extends StatelessWidget {
@@ -8,11 +10,14 @@ class OnBoardingViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Expanded(
-          child: OnBoardingPageView(
-            
+        Expanded(child: OnBoardingPageView()),
+        DotsIndicator(
+          dotsCount: 2,
+          decorator: DotsDecorator(
+            activeColor: AppColors.primaryColor,
+            color: AppColors.primaryColor.withOpacity(0.5),
           ),
-        )
+        ),
       ],
     );
   }
--- lib/features/on_boarding/presentation/views/widget/page_view_item.dart
+++ lib/features/on_boarding/presentation/views/widget/page_view_item.dart
@@ -41,10 +41,7 @@ class PageViewItem extends StatelessWidget {
         SizedBox(height: 24),
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Text(
-            subTitle,
-           textAlign: TextAlign.center
-           ),
+          child: Text(subTitle, textAlign: TextAlign.center),
         ),
       ],
     );
