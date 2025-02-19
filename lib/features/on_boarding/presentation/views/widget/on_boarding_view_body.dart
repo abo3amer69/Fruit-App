42f57c58c5031055f43b9dc116c7603cf7233203
Ahmed Amer <01099517574med@gmail.com>
2025-02-19 17:26:29 +0200

    create on boarding page view

--- lib/features/on_boarding/presentation/views/widget/on_boarding_view_body.dart
+++ lib/features/on_boarding/presentation/views/widget/on_boarding_view_body.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:fruit_app/features/on_boarding/presentation/views/widget/on_boarding_page_view.dart';
 
 class OnBoardingViewBody extends StatelessWidget {
   const OnBoardingViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        Expanded(
+          child: OnBoardingPageView(
+            
+          ),
+        )
+      ],
+    );
   }
 }
