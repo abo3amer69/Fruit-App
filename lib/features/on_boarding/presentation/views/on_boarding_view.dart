68ff36e2a72d8b6b5590757f7dd9219f0b413ba2
Ahmed Amer <01099517574med@gmail.com>
2025-02-19 17:09:45 +0200

    splash view logic done

---
+++ lib/features/on_boarding/presentation/views/on_boarding_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:fruit_app/features/on_boarding/presentation/views/widget/on_boarding_view_body.dart';
+
+class OnBoardingView extends StatelessWidget {
+  const OnBoardingView({super.key});
+
+  static const routeName = 'onoardingView';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: OnBoardingViewBody(),
+    );
+  }
+}
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -2,10 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 import 'package:fruit_app/core/utils/app_images.dart';
+import 'package:fruit_app/features/on_boarding/presentation/views/on_boarding_view.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  @override
+  void initState() {
+    exCuteNavigation();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -20,5 +31,11 @@ class SplashViewBody extends StatelessWidget {
         SvgPicture.asset(Assets.assetsImagesSplashBottom, fit: BoxFit.fill),
       ],
     );
+  }
+  
+  void exCuteNavigation() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, OnBoardingView.routeName);
+    });
   }
 }
