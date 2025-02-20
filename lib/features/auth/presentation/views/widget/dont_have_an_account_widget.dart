b2c59d86694d7611c77d00277e0317244991a499
Ahmed Amer <01099517574med@gmail.com>
2025-02-21 00:35:14 +0200

    signup ui "navigator and build app bar "

--- lib/features/auth/presentation/views/widget/dont_have_an_account_widget.dart
+++ lib/features/auth/presentation/views/widget/dont_have_an_account_widget.dart
@@ -1,12 +1,11 @@
-
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:fruit_app/core/utils/app_colors.dart';
 import 'package:fruit_app/core/utils/app_text_styles.dart';
+import 'package:fruit_app/features/auth/presentation/views/signup_view.dart';
 
 class DontHaveAnAccountWidget extends StatelessWidget {
-  const DontHaveAnAccountWidget({
-    super.key,
-  });
+  const DontHaveAnAccountWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -15,19 +14,18 @@ class DontHaveAnAccountWidget extends StatelessWidget {
         children: [
           TextSpan(
             text: 'لا تملك حساب؟',
-            style: TextStyles.semiBold16.copyWith(
-              color: Color(0xff949D9E),
-            ),
+            style: TextStyles.semiBold16.copyWith(color: Color(0xff949D9E)),
           ),
-    
+
           TextSpan(
             text: '   ',
-            style: TextStyles.semiBold16.copyWith(
-              color: Color(0xff616A6B),
-            ),
+            style: TextStyles.semiBold16.copyWith(color: Color(0xff616A6B)),
           ),
-    
+
           TextSpan(
+            recognizer: TapGestureRecognizer()..onTap = () {
+              Navigator.pushNamed(context, SignupView.routeName);
+            },
             text: 'قم بانشاء حساب',
             style: TextStyles.semiBold16.copyWith(
               color: AppColors.primaryColor,
