ea3002a4b0262ab40213c44d17d9bdd88699eb2e
Ahmed Amer <01099517574med@gmail.com>
2025-02-20 21:58:20 +0200

    login ui part 4 "text and custom button and text"

--- lib/core/widget/custom_button.dart
+++ lib/core/widget/custom_button.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fruit_app/core/utils/app_colors.dart';
+import 'package:fruit_app/core/utils/app_text_styles.dart';
 
 class CustomButton extends StatelessWidget {
   const CustomButton({super.key, required this.onPressed, required this.text});
@@ -20,7 +21,7 @@ class CustomButton extends StatelessWidget {
           backgroundColor: AppColors.primaryColor,
         ),
         onPressed: onPressed,
-        child: Text(text, style: TextStyle(color: Colors.white)),
+        child: Text(text, style: TextStyles.bold16.copyWith(color: Colors.white),),
       ),
     );
   }
