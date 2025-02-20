0e3fa5ee942ca2dbb36615105b78c4fd197c6ab8
Ahmed Amer <01099517574med@gmail.com>
2025-02-21 00:42:12 +0200

    create custom text form field "signup ui"

--- lib/features/auth/presentation/views/widget/signup_view_body.dart
+++ lib/features/auth/presentation/views/widget/signup_view_body.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:fruit_app/constants.dart';
+import 'package:fruit_app/core/widget/custom_text_form_field.dart';
 
 class SignupViewBody extends StatelessWidget {
   const SignupViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: kHorizintalPadding),
+        child: Column(
+          children: [
+            SizedBox(height: 24),
+            CustomTextFormField(
+              hintText: 'الاسم كامل',
+              textInputType: TextInputType.name,
+            ),
+            SizedBox(height: 16),
+            CustomTextFormField(
+              hintText: 'البريد الإلكتروني',
+              textInputType: TextInputType.emailAddress,
+            ),
+            SizedBox(height: 16),
+            CustomTextFormField(
+              suffixicon: Icon(Icons.remove_red_eye, color: Color(0xffC9CECF)),
+              hintText: 'كلمة المرور',
+              textInputType: TextInputType.visiblePassword,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
