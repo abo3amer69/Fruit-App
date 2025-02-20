ab49d7e6e40e6803a85c5deb0e25ce01d758a2c7
Ahmed Amer <01099517574med@gmail.com>
2025-02-20 21:33:26 +0200

    login ui part 2 "custom text form field"

--- lib/features/auth/presentation/views/widget/login_view_body.dart
+++ lib/features/auth/presentation/views/widget/login_view_body.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:fruit_app/constants.dart';
+import 'package:fruit_app/core/widget/custom_text_form_field.dart';
 
 class LoginViewBody extends StatelessWidget {
   const LoginViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kHorizintalPadding),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 24),
+            CustomTextFormField(
+              hintText: 'البريد الالكتروني',
+              textInputType: TextInputType.emailAddress,
+            ),
+            SizedBox(height: 16),
+            CustomTextFormField(
+              suffixicon: Icon(Icons.remove_red_eye, color: Color(0xffC9CECF)),
+              hintText: 'كلمة المرور',
+              textInputType: TextInputType.visiblePassword,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
