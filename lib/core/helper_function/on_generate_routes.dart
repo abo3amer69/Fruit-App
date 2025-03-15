f1e59c05dc8e5df3c773de91b409e8f4df0b075d
Ahmed Amer <01099517574med@gmail.com>
2025-03-16 01:51:02 +0200

    ceate best selling view

--- lib/core/helper_function/on_generate_routes.dart
+++ lib/core/helper_function/on_generate_routes.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fruit_app/features/auth/presentation/views/signin_view.dart';
 import 'package:fruit_app/features/auth/presentation/views/signup_view.dart';
+import 'package:fruit_app/features/best_selling_fruits/presentation/views/best_selling_view.dart';
 import 'package:fruit_app/features/home/presentation/views/home_view.dart';
 import 'package:fruit_app/features/on_boarding/presentation/views/on_boarding_view.dart';
 import 'package:fruit_app/features/splash/presentation/views/splash_view.dart';
@@ -21,6 +22,9 @@ Route onGenerateRoute(RouteSettings settings) {
 
     case HomeView.routeName:
       return MaterialPageRoute(builder: (context) => const HomeView());
+
+    case BestSellingView.routeName:
+      return MaterialPageRoute(builder: (context) => const BestSellingView());
 
     default:
       return MaterialPageRoute(builder: (context) => const Scaffold());
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -3,7 +3,7 @@ import 'package:fruit_app/constants.dart';
 import 'package:fruit_app/core/widget/fruit_item.dart';
 import 'package:fruit_app/core/widget/search_text_field.dart';
 import 'package:fruit_app/features/home/presentation/views/widgets/best_selling_grid_view.dart';
-import 'package:fruit_app/features/home/presentation/views/widgets/best_selling_headerr.dart';
+import 'package:fruit_app/features/home/presentation/views/widgets/best_selling_header.dart';
 import 'package:fruit_app/features/home/presentation/views/widgets/custom_home_app_bar.dart';
 import 'package:fruit_app/features/home/presentation/views/widgets/featured_item.dart';
 import 'package:fruit_app/features/home/presentation/views/widgets/featured_list.dart';
