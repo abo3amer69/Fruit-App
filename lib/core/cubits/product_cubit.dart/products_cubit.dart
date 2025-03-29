afb39428156d7156ccb527ef14fb3c1c125f7c21
Ahmed Amer <01099517574med@gmail.com>
2025-03-29 17:25:09 +0200

    create products cubit

---
+++ lib/core/cubits/product_cubit.dart/products_cubit.dart
@@ -0,0 +1,34 @@
+import 'package:bloc/bloc.dart';
+import 'package:fruit_app/core/entities/product_entity.dart';
+import 'package:fruit_app/core/repos/products_repo.dart';
+import 'package:meta/meta.dart';
+
+part 'products_state.dart';
+
+class ProductsCubit extends Cubit<ProductsState> {
+  ProductsCubit(this.productRepo) : super(ProductsInitial());
+
+  final ProductRepo productRepo;
+
+  Future<void> getProduct() async {
+    emit(ProductsLoading());
+
+    final result = await productRepo.getProduct();
+
+    result.fold(
+      (failure) => emit(ProductsFailure(failure.message)),
+      (products) => emit(ProductsSuccess(products)),
+    );
+  }
+
+  Future<void> getBestSellingProduct() async {
+    emit(ProductsLoading());
+
+    final result = await productRepo.getBestSellingProduct();
+
+    result.fold(
+      (failure) => emit(ProductsFailure(failure.message)),
+      (products) => emit(ProductsSuccess(products)),
+    );
+  }
+}
