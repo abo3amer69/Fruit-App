d3971942864777787775ded614e055e4b18aacf4
Ahmed Amer <01099517574med@gmail.com>
2025-04-04 19:31:11 +0200

    display number of product added

--- lib/features/home/presentation/views/widgets/cart_header.dart
+++ lib/features/home/presentation/views/widgets/cart_header.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruit_app/core/utils/app_text_styles.dart';
+import 'package:fruit_app/features/home/presentation/cubits/cart_cubit/cart_cubit.dart';
 
 class CartHeader extends StatelessWidget {
   const CartHeader({super.key});
@@ -11,7 +13,10 @@ class CartHeader extends StatelessWidget {
       padding: EdgeInsets.symmetric(vertical: 20),
       decoration: BoxDecoration(color: Color(0xffEBF9F1)),
       child: Center(
-        child: Text('لديك 3 منتجات في سله التسوق', style: TextStyles.regular13),
+        child: Text(
+          'لديك ${context.watch<CartCubit>().cartEntity.cartItems.length} منتجات في سله التسوق',
+          style: TextStyles.regular13,
+        ),
       ),
     );
   }
