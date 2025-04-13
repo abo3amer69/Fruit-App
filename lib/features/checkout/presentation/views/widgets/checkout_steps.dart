9d8d1f15b1e7130176f4fe4d09133165d021e33b
Ahmed Amer <01099517574med@gmail.com>
2025-04-13 23:59:34 +0200

    create step item(switch from active to inactive and mirror)

--- lib/features/checkout/presentation/views/widgets/checkout_steps.dart
+++ lib/features/checkout/presentation/views/widgets/checkout_steps.dart
@@ -1,7 +1,5 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:fruit_app/features/checkout/presentation/views/widgets/active_step_item.dart';
-import 'package:fruit_app/features/checkout/presentation/views/widgets/in_active_step_item.dart';
+import 'package:fruit_app/features/checkout/presentation/views/widgets/step_item.dart';
 
 class CheckoutSteps extends StatelessWidget {
   const CheckoutSteps({super.key});
@@ -10,9 +8,13 @@ class CheckoutSteps extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: List.generate(getSteps().length, (index) {
-        return Expanded(child: InActiveStepItem(
-          index: index.toString(),
-          text: getSteps()[index]));
+        return Expanded(
+          child: StepItem(
+            isActive: false,
+            index: index.toString(),
+            text: getSteps()[index],
+          ),
+        );
       }),
     );
   }
