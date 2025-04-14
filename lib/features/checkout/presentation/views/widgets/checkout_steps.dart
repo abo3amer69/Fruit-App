57ce63da9a2c5ca57d21f69304224134fc91a48a
Ahmed Amer <01099517574med@gmail.com>
2025-04-14 22:10:18 +0200

    create checkout state management

--- lib/features/checkout/presentation/views/widgets/checkout_steps.dart
+++ lib/features/checkout/presentation/views/widgets/checkout_steps.dart
@@ -2,25 +2,40 @@ import 'package:flutter/material.dart';
 import 'package:fruit_app/features/checkout/presentation/views/widgets/step_item.dart';
 
 class CheckoutSteps extends StatelessWidget {
-  const CheckoutSteps({super.key});
+  const CheckoutSteps({
+    super.key,
+    required this.currentPageIndex,
+    required this.pageController,
+  });
+
+  final int currentPageIndex;
+  final PageController pageController;
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: List.generate(getSteps().length, (index) {
         return Expanded(
-          child: StepItem(
-            isActive: false,
-            index: (index + 1).toString(),
-            text: getSteps()[index],
+          child: GestureDetector(
+            onTap: () {
+              pageController.animateToPage(
+                index,
+                duration: Duration(milliseconds: 300),
+                curve: Curves.easeIn,
+              );
+            },
+            child: StepItem(
+              isActive: index <= currentPageIndex,
+              index: (index + 1).toString(),
+              text: getSteps()[index],
+            ),
           ),
         );
       }),
     );
   }
-
-  
 }
+
 List<String> getSteps() {
-    return ['الشحن', 'العنوان', 'الدفع', ];
-  }
+  return ['الشحن', 'العنوان', 'الدفع'];
+}
--- lib/features/checkout/presentation/views/widgets/checkout_view_body.dart
+++ lib/features/checkout/presentation/views/widgets/checkout_view_body.dart
@@ -16,6 +16,11 @@ class _CheckoutViewBodyState extends State<CheckoutViewBody> {
   @override
   void initState() {
     pageController = PageController();
+    pageController.addListener(() {
+      setState(() {
+        currentPageIndex = pageController.page!.toInt();
+      });
+    });
     super.initState();
   }
 
@@ -25,6 +30,8 @@ class _CheckoutViewBodyState extends State<CheckoutViewBody> {
     super.dispose();
   }
 
+  int currentPageIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -32,22 +39,41 @@ class _CheckoutViewBodyState extends State<CheckoutViewBody> {
       child: Column(
         children: [
           SizedBox(height: 20),
-          CheckoutSteps(),
+          CheckoutSteps(
+            pageController: pageController,
+            currentPageIndex: currentPageIndex),
           Expanded(
             child: CheckoutStepsPageView(pageController: pageController),
           ),
           CustomButton(
             onPressed: () {
-              pageController.animateToPage(2,
+              pageController.animateToPage(
+                currentPageIndex + 1,
                 duration: const Duration(milliseconds: 300),
                 curve: Curves.bounceIn,
               );
             },
-            text: 'التالي',
+            text: getNextButtonText(currentPageIndex),
           ),
           SizedBox(height: 32),
         ],
       ),
     );
+  }
+
+  String getNextButtonText(int currentPageIndex) {
+    switch (currentPageIndex) {
+      case 0:
+        return 'التالي';
+
+      case 1:
+        return 'التالي';
+
+      case 2:
+        return 'الدفع عبر PayPal';
+
+      default:
+        return 'التالي';
+    }
   }
 }
