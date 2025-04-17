e53e5668648a9e2f552a9fabf2f1ae418c00731b
Ahmed Amer <01099517574med@gmail.com>
2025-04-17 16:46:24 +0200

    create entity for order and shipping adress

---
+++ lib/features/checkout/domain/entities/shipping_adress_entity.dart
@@ -0,0 +1,17 @@
+class ShippingAdressEntity {
+  final String name;
+  final String phone;
+  final String adress;
+  final String city;
+  final String email;
+  final String adressDetails;
+
+  ShippingAdressEntity(
+    this.name,
+    this.phone,
+    this.adress,
+    this.city,
+    this.email,
+    this.adressDetails,
+  );
+}
